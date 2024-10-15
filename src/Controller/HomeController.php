1306f9d07b4ea64132a45f39b326f0598803af9d
Romain GUYENOT <romain.guyenot2023@campus-eni.fr>
2024-10-15 12:06:03 +0200

    Mise en place du home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
+use App\Repository\SiteRepository;
+use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(SiteRepository $siteRepository,TripRepository $tripRepository){
+        $this->siteRepository = $siteRepository;
+        $this->tripRepository = $tripRepository;
+    }
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $sites = $this->siteRepository->findAll();
+        $trips = $this->tripRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'sites' => $sites,
+            'trips' => $trips,
         ]);
     }
 }
