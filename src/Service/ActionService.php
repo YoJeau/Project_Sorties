cb391d237af4f2690b42d115cab5dc353f1a7898
yojo <yjeauneau@gmail.com>
2024-10-18 16:58:53 +0200

    retrvaillage du code de Romain le nullos qui ne type rien

--- src/Service/ActionService.php
+++ src/Service/ActionService.php
@@ -2,13 +2,14 @@
 namespace App\Service;
 
 use App\Entity\Participant;
+use App\Entity\Trip;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ActionService
 {
-
-    public function __construct(UrlGeneratorInterface $urlGenerator){
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
         $this->urlGenerator = $urlGenerator;
     }
     /**
@@ -33,7 +34,7 @@ class ActionService
         return $actions;
     }
 
-    public function isSubscribed($user, $row): bool
+    public function isSubscribed(Participant $user, Trip $row): bool
     {
         foreach ($row->getTriSubscribes() as $subscribes) {
             if ($subscribes->getSubParticipantId()->getId() == $user->getId()) {
@@ -43,77 +44,68 @@ class ActionService
         return false;
     }
 
-    public function isOrganisator($user, $row): bool
+    public function isOrganisator(Participant $user, Trip $row): bool
     {
         return $row->getTriOrganiser()->getId() == $user->getId();
     }
 
-    public function generatePath($nameRoute,$param){
+    public function generatePath($nameRoute, $param)
+    {
         return $this->urlGenerator->generate($nameRoute, ['id' => $param], UrlGeneratorInterface::ABSOLUTE_PATH);
     }
 
-    public function chooseAction($state, $isSubcribed, $isOrganisator, $id)
+    public function chooseAction(string $state, bool $isSubcribed, bool $isOrganisator, int $id): string
     {
         // Générer le lien "Afficher" commun à tous les états
         $viewLink = "<a href='' class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Afficher</span></a>";
 
         // Vérifier l'état pour déterminer les actions possibles
-        switch ($state) {
-            case 'Ouverte':
-                return $this->getOpenStateActions($isSubcribed, $isOrganisator, $id, $viewLink);
-
-            case 'Fermée':
-                return $this->getClosedStateActions($isSubcribed, $id, $viewLink);
-
-            case 'Terminée':
-            case 'Clôturée':
-            case 'En Cours':
-            case 'Annulée':
-                return $viewLink; // Lien "Afficher" uniquement
-
-            case 'En Création':
-                return $this->getCreationStateActions($isOrganisator, $viewLink);
-
-            default:
-                return "Aucune action disponible";
-        }
+        return match ($state) {
+            'Ouverte' => $this->getOpenStateActions($isSubcribed, $isOrganisator, $id, $viewLink),
+            'Fermée' => $this->getClosedStateActions($isSubcribed, $id, $viewLink),
+            'Terminée', 'Clôturée', 'En Cours', 'Annulée' => $viewLink,
+            'En Création' => $this->getCreationStateActions($isOrganisator, $viewLink),
+            default => "Aucune action disponible",
+        };
     }
 
-    private function getOpenStateActions($isSubcribed, $isOrganisator, $id, $viewLink)
+    private function getOpenStateActions(bool  $isSubcribed, bool $isOrganisator, int $id, string $viewLink): string
     {
         if ($isSubcribed) {
             // Utilisateur inscrit
-            return $viewLink .
-                "<a href='".$this->generatePath('app_subscribe_delete', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Se désister</span></a>";
+            $viewLink .= "<a href='".$this->generatePath('app_subscribe_delete', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Se désister</span></a>";
         } elseif ($isOrganisator) {
             // Organisateur de l'événement
-            return $viewLink .
-                "<a href='" . $this->generatePath('app_trip_cancel', $id) . "'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Annuler</span></a>";
+            $viewLink .= "<a href='" . $this->generatePath('app_trip_cancel', $id) . "'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Annuler</span></a>";
         } else {
             // Pas encore inscrit
-            return $viewLink .
-                "<a href='".$this->generatePath('app_subscribe', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>S'inscrire</span></a>";
+            $viewLink .= "<a href='".$this->generatePath('app_subscribe', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>S'inscrire</span></a>";
         }
+
+        return $viewLink;
     }
 
-    private function getClosedStateActions($isSubcribed, $id, $viewLink)
+    private function getClosedStateActions(bool  $isSubcribed, int $id, string $viewLink): string
     {
         if ($isSubcribed) {
             // Inscrit à un événement fermé
-            return $viewLink .
-                "<a href='".$this->generatePath('app_subscribe_delete', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Se désister</span></a>";
+            $viewLink .= "<a href='".$this->generatePath('app_subscribe_delete', $id)."'  class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Se désister</span></a>";
         }
         // Seulement le lien "Afficher" si non inscrit
         return $viewLink;
     }
 
-    private function getCreationStateActions($isOrganisator, $viewLink)
+    private function getCreationStateActions(bool $isOrganisator, int $id): string
     {
+        $viewLink = '';
         if ($isOrganisator) {
             // L'organisateur peut modifier ou publier
-            return "<a href='' class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Modifier</span></a>".
-                "<a href='' class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Publier</span></a>";
+            $viewLink .= "<a href='' class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Modifier</span></a>".
+                "<a href='" . $this->generatePath('app_subscribe_delete', $id) . "' class='text-decoration-none'> <span class='badge rounded-pill bg-info'>Publier</span></a>";
+        } else {
+            $viewLink .= "Aucune action disponible";
         }
-        return "Aucune action disponible";
+
+        return $viewLink;
     }
 }
